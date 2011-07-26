7af834e162f77d5b590d094872d06faeeacc3ccf
Robin Kupper <robbzkupper@web.de>
2011-07-26 16:36:11 +0200

    Added some configs

--- src/de/diddiz/Superpickaxe/SPBlockListener.java
+++ src/de/diddiz/Superpickaxe/SPBlockListener.java
@@ -1,27 +1,45 @@
 package de.diddiz.Superpickaxe;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.event.block.BlockListener;
+import org.bukkit.inventory.ItemStack;
+import de.diddiz.LogBlock.Consumer;
+import de.diddiz.LogBlock.LogBlock;
 
 public class SPBlockListener extends BlockListener
 {
 	private final Superpickaxe sp;
+	private final Set<Integer> tools, dontBreak;
+	private final boolean disableDrops, disableToolWear;
+	private final Consumer consumer;
 
 	SPBlockListener(Superpickaxe sp) {
 		this.sp = sp;
+		tools = new HashSet<Integer>(sp.getConfiguration().getIntList("tools", null));
+		dontBreak = new HashSet<Integer>(sp.getConfiguration().getIntList("dontBreak", null));
+		disableDrops = sp.getConfiguration().getBoolean("disableDrops", false);
+		disableToolWear = sp.getConfiguration().getBoolean("disableToolWear", false);
+		consumer = disableToolWear ? (sp.getServer().getPluginManager().getPlugin("LogBlock") != null ? ((LogBlock)sp.getServer().getPluginManager().getPlugin("LogBlock")).getConsumer() : null) : null;
 	}
 
 	@Override
 	public void onBlockDamage(BlockDamageEvent event) {
 		final Player player = event.getPlayer();
-		if (!event.isCancelled() && sp.hasEnabled(player) && event.getItemInHand() != null) {
-			final int item = event.getItemInHand().getTypeId();
-			if (item == 270 || item == 274 || item == 278 || item == 285) {
-				if (event.getBlock().getTypeId() == 7 && !sp.hasPermission(player, "superpickaxe.breakBedrock"))
-					return;
-				event.setInstaBreak(true);
+		final ItemStack tool = event.getItemInHand();
+		if (!event.isCancelled() && sp.hasEnabled(player) && tool != null && tools.contains(tool.getTypeId()) && !(dontBreak.contains(event.getBlock().getTypeId()) && !sp.hasPermission(player, "superpickaxe.breakAll"))) {
+			if (disableDrops) {
+				if (consumer != null)
+					consumer.queueBlockBreak(player.getName(), event.getBlock().getState());
+				event.getBlock().setTypeId(0);
+				event.setCancelled(true);
+				return;
 			}
+			event.setInstaBreak(true);
+			if (disableToolWear)
+				tool.setDurability((short)(tool.getDurability() - 1));
 		}
 	}
 }
--- src/de/diddiz/Superpickaxe/Superpickaxe.java
+++ src/de/diddiz/Superpickaxe/Superpickaxe.java
@@ -1,6 +1,10 @@
 package de.diddiz.Superpickaxe;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.Command;
@@ -11,19 +15,32 @@ import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
 import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class Superpickaxe extends JavaPlugin
 {
-	private PermissionHandler permissions;
+	private PermissionHandler permissions = null;
 	private final HashSet<String> playerswithsp = new HashSet<String>();
 
 	@Override
 	public void onEnable() {
 		final PluginManager pm = getServer().getPluginManager();
-		permissions = ((Permissions)pm.getPlugin("Permissions")).getHandler();
-		getConfiguration().load();
+		if (pm.getPlugin("Permissions") != null)
+			permissions = ((Permissions)pm.getPlugin("Permissions")).getHandler();
+		final Map<String, Object> def = new HashMap<String, Object>();
+		def.put("overrideWorldEditCommands", false);
+		def.put("tools", Arrays.asList(270, 274, 278, 285));
+		def.put("dontBreak", Arrays.asList(7));
+		def.put("disableDrops", false);
+		def.put("disableToolWear", false);
+		final Configuration config = getConfiguration();
+		config.load();
+		for (final Entry<String, Object> e : def.entrySet())
+			if (config.getProperty(e.getKey()) == null)
+				config.setProperty(e.getKey(), e.getValue());
+		config.save();
 		final SPPlayerListener playerListener = new SPPlayerListener(this);
 		pm.registerEvent(Type.BLOCK_DAMAGE, new SPBlockListener(this), Priority.Normal, this);
 		pm.registerEvent(Type.PLAYER_PORTAL, playerListener, Priority.Monitor, this);
@@ -46,7 +63,7 @@ public class Superpickaxe extends JavaPlugin
 			return true;
 		}
 		final Player player = (Player)sender;
-		if (!permissions.has(player, "superpickaxe.use")) {
+		if (!hasPermission(player, "superpickaxe.use")) {
 			player.sendMessage(ChatColor.RED + "You aren't allowed to do this.");
 			return true;
 		}

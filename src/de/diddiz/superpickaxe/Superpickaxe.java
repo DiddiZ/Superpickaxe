67c1b8d374aa42ea9155f695fd7f91277560285c
DiddiZ <robbzkupper@web.de>
2012-03-07 00:55:20 +0100

    Updated to 1.2

--- src/de/diddiz/superpickaxe/Superpickaxe.java
+++ src/de/diddiz/superpickaxe/Superpickaxe.java
@@ -1,6 +1,5 @@
 package de.diddiz.superpickaxe;
 
-import static org.bukkit.Bukkit.getLogger;
 import static org.bukkit.Bukkit.getPluginCommand;
 import static org.bukkit.Bukkit.getPluginManager;
 import java.util.Arrays;
@@ -12,19 +11,30 @@ import java.util.Set;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.event.Event.Type;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
+import de.diddiz.LogBlock.Consumer;
+import de.diddiz.LogBlock.LogBlock;
 
-public class Superpickaxe extends JavaPlugin
+public class Superpickaxe extends JavaPlugin implements Listener
 {
 	private PermissionHandler permissions = null;
 	private final Set<String> playerswithsp = new HashSet<String>();
+	private Set<Integer> tools, dontBreak;
+	private boolean disableDrops, disableToolWear, overrideWorldEditCommands;
+	private Consumer consumer;
 
 	@Override
 	public void onEnable() {
@@ -42,15 +52,27 @@ public class Superpickaxe extends JavaPlugin
 			if (!config.contains(e.getKey()))
 				config.set(e.getKey(), e.getValue());
 		saveConfig();
-		final SPPlayerListener playerListener = new SPPlayerListener(this);
-		pm.registerEvent(Type.BLOCK_DAMAGE, new SPBlockListener(this), Priority.Normal, this);
-		pm.registerEvent(Type.PLAYER_CHANGED_WORLD, playerListener, Priority.Monitor, this);
-		if (getConfig().getBoolean("overrideWorldEditCommands")) {
+		final ConfigurationSection cfg = getConfig();
+		tools = new HashSet<Integer>(cfg.getIntegerList("tools"));
+		dontBreak = new HashSet<Integer>(cfg.getIntegerList("dontBreak"));
+		disableDrops = cfg.getBoolean("disableDrops", false);
+		disableToolWear = cfg.getBoolean("disableToolWear", false);
+		overrideWorldEditCommands = cfg.getBoolean("overrideWorldEditCommands");
+		if (disableDrops) {
+			if (getPluginManager().isPluginEnabled("LogBlock"))
+				consumer = ((LogBlock)getPluginManager().getPlugin("LogBlock")).getConsumer();
+			else {
+				getLogger().severe("[Superpickaxe] LogBlock not found");
+				consumer = null;
+			}
+		} else
+			consumer = null;
+		pm.registerEvents(this, this);
+		if (overrideWorldEditCommands) {
 			getLogger().info("[Superpickaxe] Overriding WorldEdit commands");
 			for (final String cmd : new String[]{"/", "superpickaxe"})
 				if (getPluginCommand(cmd) != null)
 					getPluginCommand(cmd).setExecutor(this);
-			pm.registerEvent(Type.PLAYER_COMMAND_PREPROCESS, playerListener, Priority.Lowest, this);
 		}
 		getLogger().info("Superpickaxe v" + getDescription().getVersion() + " by DiddiZ enabled");
 	}
@@ -74,6 +96,41 @@ public class Superpickaxe extends JavaPlugin
 		} else
 			sender.sendMessage("You aren't a player");
 		return true;
+	}
+
+	@EventHandler
+	public void onBlockDamage(BlockDamageEvent event) {
+		final Player player = event.getPlayer();
+		final ItemStack tool = event.getItemInHand();
+		if (!event.isCancelled() && hasEnabled(player) && tool != null && tools.contains(tool.getTypeId()) && !(dontBreak.contains(event.getBlock().getTypeId()) && !hasPermission(player, "superpickaxe.breakAll")))
+			if (disableDrops && consumer != null) {
+				consumer.queueBlockBreak(player.getName(), event.getBlock().getState());
+				event.getBlock().setTypeId(0);
+				event.setCancelled(true);
+			} else {
+				event.setInstaBreak(true);
+				if (disableToolWear)
+					tool.setDurability((short)(tool.getDurability() - 1));
+			}
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+		if (!event.isCancelled() && overrideWorldEditCommands) {
+			final String msg = event.getMessage().toLowerCase();
+			if (msg.equals("/") || msg.equals("//") || msg.equals("/,") || msg.equals("/sp")) {
+				event.setMessage("dummy");
+				event.setCancelled(true);
+				getServer().dispatchCommand(event.getPlayer(), "spa");
+			}
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
+		final Player player = event.getPlayer();
+		if (hasEnabled(player) && !hasPermission(player, "superpickaxe.use"))
+			removePlayer(player);
 	}
 
 	void addPlayer(Player player) {
